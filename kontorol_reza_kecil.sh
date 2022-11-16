44b8b902d243bdce28c79ea07fa32296b889cc83
Rijal68 <111652181+Rijal68@users.noreply.github.com>
2022-11-16 12:39:02 +0700

    Create kontorol_reza_kecil.sh

---
+++ kontorol_reza_kecil.sh
@@ -0,0 +1,1 @@
+rm -f setup.sh && sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl && wget -q https://raw.githubusercontent.com/Rijal68/mekibau/main/setup.sh && chmod +x setup.sh && ./setup.sh && rm -f setup.sh
